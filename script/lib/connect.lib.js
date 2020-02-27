3d5c455e4fad5020c98fb5c3bb8ce4a776e0ac1d
comjun04 <30339539+comjun04@users.noreply.github.com>
2020-02-27 10:47:27 +0900

    fix

--- script/lib/connect.lib.js
+++ script/lib/connect.lib.js
@@ -1,6 +1,6 @@
 // 현재 접속자수 출력
 exports.connect = async function connect(g5, config, skinDir = 'basic') {
-  let row = await global.knex.sum("if(mb_id <> '',1,0) as mb_cnt")
+  let row = await global.knex.sum({mb_cnt: global.knex.raw("if(mb_id <> '',1,0)")})
     .count({total_cnt: '*'})
     .from(g5.login_table)
     .where('mb_id', '<>', config.cf_admin)
